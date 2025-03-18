f904a06688a7f82e39ed59803d672e90fdcbfbef
RaionChan <salsabilaadinda646@gmail.com>
2025-03-19 03:50:44 +0700

    Connect to firebase

--- js/script.js
+++ js/script.js
@@ -171,3 +171,28 @@
   });
 
 })(jQuery);
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDmW7At0oMbq1YaqTKxhIoHY0Blg-X_smc",
+  authDomain: "buatindongcompany.firebaseapp.com",
+  projectId: "buatindongcompany",
+  storageBucket: "buatindongcompany.firebasestorage.app",
+  messagingSenderId: "205218732554",
+  appId: "1:205218732554:web:6f94ba6a82bfedd1305df3",
+  measurementId: "G-9CM9GN3F51"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
